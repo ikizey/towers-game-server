6a0864576c1ea2d8ba17719a7998cd38c5d35926
kizey <ikizey@icloud.com>
2022-12-02 19:19:51 +0400

    remove io

--- socketHandlers/connectionHandler.js
+++ socketHandlers/connectionHandler.js
@@ -2,15 +2,14 @@ const { connectHandler } = require('./connectHandler');
 const { disconnectHandler } = require('./disconnectHandler');
 const { gameHandler } = require('./gameHandler');
 const { queueHandler } = require('./queueHandler');
-const { io } = require('../index');
 
 const connectionHandler = (client) => {
   console.info('client connected: ' + client.id);
 
   connectHandler(client);
-  gameHandler(io, client);
-  queueHandler(io, client);
   disconnectHandler(client);
+  gameHandler(client);
+  queueHandler(client);
 };
 
 module.exports = { connectionHandler };
