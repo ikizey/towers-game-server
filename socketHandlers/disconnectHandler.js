d6b4d207fd39f54ac75ec55ed68490c862504abc
kizey <ikizey@icloud.com>
2022-12-02 17:05:16 +0400

    add remove from queue on disconnect

--- socketHandlers/disconnectHandler.js
+++ socketHandlers/disconnectHandler.js
@@ -1,10 +1,11 @@
+const { queuesController } = require('../controllers/QueuesController');
 let { totalPlayers } = require('../globals');
 
 const disconnectHandler = (client) => {
   client.on('disconnect', () => {
     totalPlayers -= 1;
 
-    //TODO remove from all queues
+    queuesController.remove(client);
     //TODO concede in game (if exist)
     console.info('client disconnected: ' + client.id);
   });
