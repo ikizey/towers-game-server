8fa19a8f05454e500cdf939348fd05aa1b941764
kizey <ikizey@icloud.com>
2022-12-08 01:24:17 +0400

    cleanup

--- socketHandlers/disconnectHandler.js
+++ socketHandlers/disconnectHandler.js
@@ -1,15 +1,14 @@
 const { queuesController } = require('../controllers/QueuesController');
 const clientsController = require('../controllers/ClientController');
 const { preGameController } = require('../controllers/PreGameController');
-let { totalPlayers, players } = require('../globals');
 
 const disconnectHandler = (client) => {
   client.on('disconnect', () => {
-    totalPlayers -= 1;
     queuesController.remove(client);
     clientsController.remove(client);
     preGameController.removeClient(client.id);
 
+    client.gameController = null;
     //TODO concede in game (if exist)
     console.info('client disconnected: ' + client.uid);
   });
